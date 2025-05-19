8f648345223b753c25401a160d0e8f3577964641
SamaraDcruz <samaragracedcruz@gmail.com>
2025-05-19 01:50:43 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+   # Ask the user for 2 numbers
+num1 = float(input("Enter  the first number:  "))
+num2 = float(input("Enter the second number:  "))
+num3 = float(input("Enter the third number:   "))
+
+# Calculate the average
+average = (num1 + num2+ num3)  / 3
+
+# Show the result
+print("The average is:", average)
+
